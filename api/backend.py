a3177f38bfa11ad926966c1bf700fef248cc7b6a
Chiara Esposito <48214920+ChiaraEsposito@users.noreply.github.com>
2022-01-23 19:18:31 +0100

    Add files via upload

--- api/backend.py
+++ api/backend.py
@@ -2,6 +2,7 @@ import flask
 
 from flask import request, jsonify, render_template
 import os
+import requests
 
 IMAGE_FOLDER = os.path.join('static', 'images')
 
@@ -16,11 +17,14 @@ from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 from pm4py.visualization.dfg import visualizer as dfg_visualization
 
+import pm4py
+from pm4py.objects.dfg.filtering import dfg_filtering
 
+log = xes_importer.apply('event logs\\running-example.xes')
     
 @app.route('/dfgFrequency', methods=['GET'])
 def dfgFrequency():
-    log = xes_importer.apply('C:\\Users\\lucia\\Downloads\\running-example.xes')
+    
     
     #DFG - process discovery
     dfg_freq = dfg_discovery.apply(log)
@@ -38,7 +42,6 @@ def dfgFrequency():
     
 @app.route('/dfgPerformance', methods=['GET'])
 def dfgPerformance():
-    log = xes_importer.apply('C:\\Users\\lucia\\Downloads\\running-example.xes')
     
     #DFG - process discovery
     dfg_perf = dfg_discovery.apply(log, variant=dfg_discovery.Variants.PERFORMANCE)
@@ -56,5 +59,70 @@ def dfgPerformance():
     #performance = render_template("img_perf.html", img_perf = perf_img)
     
     return str(gviz_perf)
+
+@app.route('/dfgFreqReduced', methods=['GET', 'POST'])
+def dfgFreqReduced():
+
+    #print(type(request.args.get('myPahtF')))
+    
+    # GET
+    #print(type(request.args.get('myPathF')))
+    #x = request.args.get('myPathF')
+    act = int(request.args.get('myActF')) 
+    path = int(request.args.get('myPathF'))
+    
+    dfg_f, sa_f, ea_f = pm4py.discover_directly_follows_graph(log)
+    activities_count_f = pm4py.get_event_attribute_values(log, "concept:name")
+    dfg_f, sa_f, ea_f, activities_count_f = dfg_filtering.filter_dfg_on_activities_percentage(dfg_f, sa_f, ea_f, activities_count_f, act/100)
+    dfg_f, sa_f, ea_f, activities_count_f = dfg_filtering.filter_dfg_on_paths_percentage(dfg_f, sa_f, ea_f, activities_count_f, path/100)
+    gviz_f = dfg_visualization.apply(dfg_f, log=log, variant=dfg_visualization.Variants.FREQUENCY)
+    
+    return str(gviz_f)
+
+@app.route('/dfgPerfReduced', methods=['GET', 'POST'])
+def dfgPerfReduced():
+
+    #print(type(request.args.get('myPahtF')))
+    
+    # GET
+    #print(type(request.args.get('myPathF')))
+    #x = request.args.get('myPathF')
+    act = int(request.args.get('myActP')) 
+    path = int(request.args.get('myPathP'))
+    
+    dfg_p, sa_p, ea_p = pm4py.discover_directly_follows_graph(log)
+    activities_count_p = pm4py.get_event_attribute_values(log, "concept:name")
+    dfg_p, sa_p, ea_p, activities_count_p = dfg_filtering.filter_dfg_on_activities_percentage(dfg_p, sa_p, ea_p, activities_count_p, act/100)
+    dfg_p, sa_p, ea_p, activities_count_p = dfg_filtering.filter_dfg_on_paths_percentage(dfg_p, sa_p, ea_p, activities_count_p, path/100)
+    parameters = {dfg_visualization.Variants.PERFORMANCE.value.Parameters.FORMAT: "svg"}
+    gviz_f = dfg_visualization.apply(dfg_p, log=log, variant=dfg_visualization.Variants.PERFORMANCE, parameters=parameters)
+    
+    return str(gviz_f)
+    
+@app.route('/median', methods=['GET', 'POST'])
+def median():
+    #MEDIAN CASE
+    from pm4py.statistics.traces.generic.log import case_statistics
+    import time
+    
+    median_case_duration = case_statistics.get_median_case_duration(log, parameters={
+        case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"
+    })
+    return str(median_case_duration)
+    
+@app.route('/total', methods=['GET', 'POST'])
+def total():
+    #ALL CASES
+    from pm4py.statistics.traces.generic.log import case_statistics
+    import time
+    
+    all_case_durations = case_statistics.get_all_case_durations(log, parameters={
+        case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"})
+    total = 0
+    for i in range(0, len(all_case_durations)):
+       total = total + all_case_durations[i];
+
+    return str(total)
+
     
 app.run(host='127.0.0.1', port=7777)
--- api/frontend.py
+++ api/frontend.py
@@ -2,6 +2,8 @@ import flask
 
 from flask import request, jsonify, render_template
 import os
+import sys
+
 
 IMAGE_FOLDER = os.path.join('static', 'images')
 
@@ -13,28 +15,65 @@ import requests
 import graphviz
 from graphviz import Digraph
 from pm4py.visualization.dfg import visualizer as dfg_visualization
+
   
 @app.route('/', methods=['GET'])
-def myfunction():
+def home():
     
     freq =requests.get('http://127.0.0.1:7777/dfgFrequency')
     perf =requests.get('http://127.0.0.1:7777/dfgPerformance')
+    median =requests.get('http://127.0.0.1:7777/median')
+    total =requests.get('http://127.0.0.1:7777/total')
+        
     
-    freq_png = graphviz.Source(freq.text, filename="frequency", format="png")
-    dfg_visualization.save(freq_png, "static/images/frequency.png")
-    freq_img = os.path.join(app.config['UPLOAD_FOLDER'], 'frequency.png')
+    #print(total.text)
+    #freq_png = graphviz.Source(freq.text, filename="frequency", format="png")
+    #dfg_visualization.save(freq_png, "static/images/frequency.png")
+    #freq_img = os.path.join(app.config['UPLOAD_FOLDER'], 'frequency.png')
 
-    perf_png = graphviz.Source(perf.text, filename="performance", format="png")
-    dfg_visualization.save(perf_png, "static/images/performance.png")
-    perf_img = os.path.join(app.config['UPLOAD_FOLDER'], 'performance.png')
+    #perf_png = graphviz.Source(perf.text, filename="performance", format="png")
+    #dfg_visualization.save(perf_png, "static/images/performance.png")
+    #perf_img = os.path.join(app.config['UPLOAD_FOLDER'], 'performance.png')
     
-    
- 
-    return render_template("index.html", img_freq = freq_img, img_perf = perf_img, string = str(perf.text))
+    return render_template("index.html", stringF = str(freq.text), stringP = str(perf.text), median = median.text, total = total.text)
     #return render_template("viz-js.html")
     
     
     #return "<h1>This is just an example</h1><p>Here, you should see the Digraph in a String format.</p><img src='dfg.png'>" 
 
+#@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['POST'])
+def myPost():
+    
+    #if request.method == 'POST' and request.form.get("frequency") != '':
+    if request.form.get("tabType") == 'frequency':
+        if request.form.get('myPathF') != '':
+            myPathF = request.form.get('myPathF')
+            myActF = request.form.get('myActF')
+            
+            #path = request.args.get('myPathF')
+           
+            params = {'myPathF' : myPathF, 'myActF' : myActF}
+            f = requests.get('http://127.0.0.1:7777/dfgFreqReduced', params = params)
+            perf =requests.get('http://127.0.0.1:7777/dfgPerformance')
+           
+            #f = requests.post('http://127.0.0.1:7777/dfgFreqReduced', path)
+            #f = requests.get('http://127.0.0.1:7777/dfgFreqReduced?myPahtF='+path)
+            return render_template("index.html", stringF = str(f.text), stringP = str(perf.text))
+            
+    if request.form.get("tabType") == 'performance':
+        if request.form.get('myPathP') != '':
+            myPathP = request.form.get('myPathP')
+            myActP = request.form.get('myActP')
+            
+            #path = request.args.get('myPathF')
+           
+            params = {'myPathP' : myPathP, 'myActP' : myActP}
+            p = requests.get('http://127.0.0.1:7777/dfgPerfReduced', params = params)
+            freq =requests.get('http://127.0.0.1:7777/dfgFrequency')
+           
+            #f = requests.post('http://127.0.0.1:7777/dfgFreqReduced', path)
+            #f = requests.get('http://127.0.0.1:7777/dfgFreqReduced?myPahtF='+path)
+            return render_template("index.html", stringF = str(freq.text), stringP = str(p.text))        
 
 app.run(host='127.0.0.1', port=8080)
