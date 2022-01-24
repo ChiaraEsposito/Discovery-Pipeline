782314d4605abf43e07e2789264d4ece3fbfe5e3
Chiara Esposito <48214920+ChiaraEsposito@users.noreply.github.com>
2022-01-24 10:07:33 +0100

    Add files via upload

--- api/backend.py
+++ api/backend.py
@@ -20,11 +20,11 @@ from pm4py.visualization.dfg import visualizer as dfg_visualization
 import pm4py
 from pm4py.objects.dfg.filtering import dfg_filtering
 
-log = xes_importer.apply('event logs\\running-example.xes')
+#log = xes_importer.apply('event logs\\running-example.xes')
     
 @app.route('/dfgFrequency', methods=['GET'])
 def dfgFrequency():
-    
+    log = xes_importer.apply('event logs\\running-example.xes')    
     
     #DFG - process discovery
     dfg_freq = dfg_discovery.apply(log)
@@ -42,7 +42,7 @@ def dfgFrequency():
     
 @app.route('/dfgPerformance', methods=['GET'])
 def dfgPerformance():
-    
+    log = xes_importer.apply('event logs\\running-example.xes')   
     #DFG - process discovery
     dfg_perf = dfg_discovery.apply(log, variant=dfg_discovery.Variants.PERFORMANCE)
     parameters = {dfg_visualization.Variants.PERFORMANCE.value.Parameters.FORMAT: "svg"}
@@ -62,15 +62,21 @@ def dfgPerformance():
 
 @app.route('/dfgFreqReduced', methods=['GET', 'POST'])
 def dfgFreqReduced():
-
+    log = xes_importer.apply('event logs\\running-example.xes')
     #print(type(request.args.get('myPahtF')))
     
     # GET
     #print(type(request.args.get('myPathF')))
     #x = request.args.get('myPathF')
-    act = int(request.args.get('myActF')) 
-    path = int(request.args.get('myPathF'))
-    
+    if request.args.get('myActF') == None:
+        act = 100;
+    else:
+        act = int(request.args.get('myActF'))
+    if request.args.get('myPathF') == None:
+        path = 100;
+    else:
+        path = int(request.args.get('myPathF'))
+    print("Freq: "+str(act)+" "+str(path))
     dfg_f, sa_f, ea_f = pm4py.discover_directly_follows_graph(log)
     activities_count_f = pm4py.get_event_attribute_values(log, "concept:name")
     dfg_f, sa_f, ea_f, activities_count_f = dfg_filtering.filter_dfg_on_activities_percentage(dfg_f, sa_f, ea_f, activities_count_f, act/100)
@@ -81,15 +87,21 @@ def dfgFreqReduced():
 
 @app.route('/dfgPerfReduced', methods=['GET', 'POST'])
 def dfgPerfReduced():
-
+    log = xes_importer.apply('event logs\\running-example.xes')
     #print(type(request.args.get('myPahtF')))
     
     # GET
     #print(type(request.args.get('myPathF')))
     #x = request.args.get('myPathF')
-    act = int(request.args.get('myActP')) 
-    path = int(request.args.get('myPathP'))
-    
+    if request.args.get('myActP') == None:
+        act = 100;
+    else:
+        act = int(request.args.get('myActP'))
+    if request.args.get('myPathP') == None:
+        path = 100;
+    else:
+        path = int(request.args.get('myPathP'))
+    print("Perf: "+str(act)+" "+str(path))
     dfg_p, sa_p, ea_p = pm4py.discover_directly_follows_graph(log)
     activities_count_p = pm4py.get_event_attribute_values(log, "concept:name")
     dfg_p, sa_p, ea_p, activities_count_p = dfg_filtering.filter_dfg_on_activities_percentage(dfg_p, sa_p, ea_p, activities_count_p, act/100)
@@ -104,7 +116,7 @@ def median():
     #MEDIAN CASE
     from pm4py.statistics.traces.generic.log import case_statistics
     import time
-    
+    log = xes_importer.apply('event logs\\running-example.xes')
     median_case_duration = case_statistics.get_median_case_duration(log, parameters={
         case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"
     })
@@ -115,7 +127,7 @@ def total():
     #ALL CASES
     from pm4py.statistics.traces.generic.log import case_statistics
     import time
-    
+    log = xes_importer.apply('event logs\\running-example.xes')
     all_case_durations = case_statistics.get_all_case_durations(log, parameters={
         case_statistics.Parameters.TIMESTAMP_KEY: "time:timestamp"})
     total = 0
--- api/frontend.py
+++ api/frontend.py
@@ -1,15 +1,16 @@
 import flask
 
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, redirect
 import os
 import sys
 
 
-IMAGE_FOLDER = os.path.join('static', 'images')
+#IMAGES_FOLDER = os.path.join('static', 'images')
+LOGS_FOLDER = "/event logs"
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
+app.config['UPLOAD_FOLDER'] = LOGS_FOLDER
 
 import requests
 import graphviz
@@ -35,7 +36,17 @@ def home():
     #dfg_visualization.save(perf_png, "static/images/performance.png")
     #perf_img = os.path.join(app.config['UPLOAD_FOLDER'], 'performance.png')
     
-    return render_template("index.html", stringF = str(freq.text), stringP = str(perf.text), median = median.text, total = total.text)
+    return render_template("index.html", \
+        stringF = str(freq.text), \
+        stringP = str(perf.text), \
+        median = median.text, \
+        total = total.text, \
+        myPathF_init = "100", \
+        myActF_init = "100", \
+        myPathP_init = "100", \
+        myActP_init = "100", \
+        perf_checked = "false") 
+        
     #return render_template("viz-js.html")
     
     
@@ -44,7 +55,50 @@ def home():
 #@app.route('/', methods=['GET','POST'])
 @app.route('/', methods=['POST'])
 def myPost():
+    median =requests.get('http://127.0.0.1:7777/median')
+    total =requests.get('http://127.0.0.1:7777/total')
+        
+        
+    myPathP = request.form.get('myPathP')
+    myActP = request.form.get('myActP')
+    myPathF = request.form.get('myPathF')
+    myActF = request.form.get('myActF')
+    perfCheck = request.form.get('perf_checked')
     
+    if perfCheck == None:
+        perfCheck = "false";
+    else:
+        perfCheck = "true";
+        
+    
+    #path = request.args.get('myPathF')
+    paramsF = {'myPathF' : myPathF, 'myActF' : myActF}
+    paramsP = {'myPathP' : myPathP, 'myActP' : myActP}
+    f = requests.get('http://127.0.0.1:7777/dfgFreqReduced', params = paramsF)
+    p = requests.get('http://127.0.0.1:7777/dfgPerfReduced', params = paramsP)
+    
+   
+    #f = requests.post('http://127.0.0.1:7777/dfgFreqReduced', path)
+    #f = requests.get('http://127.0.0.1:7777/dfgFreqReduced?myPahtF='+path)
+    if request.form.get('updated') != None:
+        f = request.files['file']
+        if f.filename != '': 
+          #f.save("event logs/" + f.filename)
+          f.save("event logs/running-example.xes")
+          return home()      
+      
+    return render_template("index.html", \
+        stringF = str(f.text), \
+        stringP = str(p.text),\
+        median = median.text, \
+        total = total.text, \
+        myPathF_init = myPathF, \
+        myActF_init = myActF, \
+        myPathP_init = myPathP, \
+        myActP_init = myActP, \
+        perf_checked = perfCheck )
+        
+    '''
     #if request.method == 'POST' and request.form.get("frequency") != '':
     if request.form.get("tabType") == 'frequency':
         if request.form.get('myPathF') != '':
@@ -59,21 +113,53 @@ def myPost():
            
             #f = requests.post('http://127.0.0.1:7777/dfgFreqReduced', path)
             #f = requests.get('http://127.0.0.1:7777/dfgFreqReduced?myPahtF='+path)
-            return render_template("index.html", stringF = str(f.text), stringP = str(perf.text))
+            return render_template("index.html", \
+                stringF = str(f.text), \
+                stringP = str(perf.text), \
+                median = median.text, \
+                total = total.text, \
+                myPathF_init = myPathF, \
+                myActF_init = myActF, \
+                myPathP_init = "0", \
+                myActP_init = "100" )
             
     if request.form.get("tabType") == 'performance':
         if request.form.get('myPathP') != '':
             myPathP = request.form.get('myPathP')
             myActP = request.form.get('myActP')
+            myPathF = request.form.get('myPathF')
+            myActF = request.form.get('myActF')
             
             #path = request.args.get('myPathF')
-           
-            params = {'myPathP' : myPathP, 'myActP' : myActP}
-            p = requests.get('http://127.0.0.1:7777/dfgPerfReduced', params = params)
-            freq =requests.get('http://127.0.0.1:7777/dfgFrequency')
+            paramsF = {'myPathP' : myPathF, 'myActP' : myActF}
+            paramsP = {'myPathP' : myPathP, 'myActP' : myActP}
+            f = requests.get('http://127.0.0.1:7777/dfgFreqReduced', params = paramsF)
+            p = requests.get('http://127.0.0.1:7777/dfgPerfReduced', params = paramsP)
+            
            
             #f = requests.post('http://127.0.0.1:7777/dfgFreqReduced', path)
             #f = requests.get('http://127.0.0.1:7777/dfgFreqReduced?myPahtF='+path)
-            return render_template("index.html", stringF = str(freq.text), stringP = str(p.text))        
+            return render_template("index.html", \
+                stringF = str(f.text), \
+                stringP = str(p.text),\
+                median = median.text, \
+                total = total.text, \
+                myPathF_init = myPathF, \
+                myActF_init = myActF, \
+                myPathP_init = myPathP, \
+                myActP_init = myActP )
+    '''      
 
+
+@app.route('/uploader', methods = ['POST'])
+def upload_file():
+  f = request.files['file']
+  if f.filename == '':
+    print("empty")
+  #f.save("event logs/" + f.filename)
+  #f.save("event logs/running-example.xes")
+  return redirect("http://127.0.0.1:8080", code=200)
+      
+      
+      
 app.run(host='127.0.0.1', port=8080)
